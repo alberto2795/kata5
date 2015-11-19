003dccbdab986df06f15683996eefcca3f99cf41
usuario <usuario@10.22.143.7>
2015-11-19 09:51:26 +0000

    VERSION 3 pone los emails del archivo en la base de datos

--- src/kata5v1.java
+++ src/kata5v1.java
@@ -1,9 +1,14 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
 
 public class kata5v1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         //Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection connection = DriverManager.getConnection(
@@ -21,6 +26,16 @@ public class kata5v1 {
         }
         
         rs.close();
+        
+        String namefile = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\kata 5\\emailsfilev1.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(namefile)));
+        String mail;
+        while ((mail=reader.readLine())!=null){
+            query = "INSERT INTO MAILS (MAIL) VALUES ('" + mail + "')";
+            state.executeUpdate(query);
+        }
+        
+        
         state.close();
         connection.close();
     }
